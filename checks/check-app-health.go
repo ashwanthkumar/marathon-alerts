408f173f47b17f6c30e16d13cc58b1e5b3f1dbb2
seeker815 <ephemeral972@gmail.com>
2017-12-25 02:03:37 +0530

    Check for http protocol for app

--- checks/check-app-health.go
+++ checks/check-app-health.go
@@ -25,28 +25,34 @@ func (h *AppHealth) Check(app marathon.Application) AppCheck {
 	hostList := maps.GetString(app.Labels, "router.hosts", "notDefined")
 	hostSlice := strings.Split(hostList, ":")
 	host := hostSlice[0]
-	hostPath := app.HealthChecks[0].Path
-	hostURL := strings.Join([]string{"http://", host, hostPath}, "")
+	healthCheckPath := app.HealthChecks[0].Path
+	healthCheckProtocol := app.HealthChecks[0].Protocol
+	hostURL := strings.Join([]string{"http://", host, healthCheckPath}, "")
 	result := Pass
 	message := fmt.Sprintf("HTTP Status OK!!")
 
-	resp, err := http.Get(hostURL)
-	if err != nil {
-		log.Println(log.Ldate|log.Ltime, "ERROR:", err)
-	} else {
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
+	if healthCheckProtocol == "HTTP" {
+		resp, err := http.Get(hostURL)
 		if err != nil {
 			log.Println(log.Ldate|log.Ltime, "ERROR:", err)
-		}
-		responseString := string(body)
-
-		if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-			result = Pass
 		} else {
-			result = Critical
-			message = fmt.Sprintf("HTTP Response Status: %s  %s ", strconv.Itoa(resp.StatusCode), responseString)
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println(log.Ldate|log.Ltime, "ERROR:", err)
+			}
+			responseString := string(body)
+
+			if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+				result = Pass
+			} else {
+				result = Critical
+				message = fmt.Sprintf("HTTP Response Status: %s  %s ", strconv.Itoa(resp.StatusCode), responseString)
+			}
 		}
+	} else {
+		result = Warning
+		message = fmt.Sprintf("The healtcheck can be run for an App with HTTP endpoint!!!")
 	}
 	return AppCheck{
 		App:       app.ID,
